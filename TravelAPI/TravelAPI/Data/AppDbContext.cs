5c2f2886420e6639b29d138c3c459d7612777c21
JoelFaria <joel.o.faria25@gmail.com>
2025-12-06 22:51:38 +0000

    Add Packages model and DbSet to AppDbContext
    Introduces the Packages entity with properties for name, base price, dates, and destination relationship. Registers Packages as a DbSet in AppDbContext to enable database operations for travel packages.

--- TravelAPI/TravelAPI/Data/AppDbContext.cs
+++ TravelAPI/TravelAPI/Data/AppDbContext.cs
@@ -8,5 +8,6 @@ namespace TravelAPI.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Packages> Packages { get; set; }
     }
 }
