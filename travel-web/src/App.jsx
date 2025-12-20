57cd815fcb7876d0eab6a6b36be56bc623c947ff
JoelFaria <joel.o.faria25@gmail.com>
2025-12-20 17:06:08 +0000

    Initialize React + Vite travel-web project
    Set up a new React project using Vite, including initial configuration files, ESLint setup, basic index.html, and starter source files. Added dependencies for React, React Router, Bootstrap, Axios, and ESLint plugins.

---
+++ travel-web/src/App.jsx
@@ -0,0 +1,20 @@
+
+function App() {
+ 
+  return(
+    <div className="container mt-5">
+      <div className="card text-center">
+        <div className="card-header">
+          Sistema de Viagens
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">Bem-vindo ao Sistema de Viagens</h5>
+          <p className="card-text">Este é o sistema de gerenciamento de viagens.</p>
+          <button className="btn btn-primary">Iniciar</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ travel-web/src/services/api.jsx
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+const api_url = 'https://localhost:7139/api';
